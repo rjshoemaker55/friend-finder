4a9deaed38ffdb2ce8d5ef5b3ae23fdd354c1831
rjshoemaker55 <rjshoemaker55@gmail.com>
2019-05-21 09:53:29 -0400

    removed home page, root is survey

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -12,6 +12,6 @@ module.exports = function(app) {
   })
 
   app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, '../public/home.html'))
+    res.sendFile(path.join(__dirname, '../public/survey.html'))
   })
 }
